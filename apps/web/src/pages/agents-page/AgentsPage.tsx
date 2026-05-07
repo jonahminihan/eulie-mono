96997c29a29973cff056762c340f25087b81aa0b
Jonah Minihan <jonahminihan@gmail.com>
2026-05-06 22:16:07 -0700

    Beginning work on docking solution.

--- apps/web/src/pages/agents-page/AgentsPage.tsx
+++ apps/web/src/pages/agents-page/AgentsPage.tsx
@@ -1,12 +1,15 @@
 import { AgentsProvider } from "@/contexts/AgentsContext";
 import AgentAppFrame from "../../components/agent-app-frame/AgentAppFrame";
 import AgentChat from "../../components/agent-chat/AgentChat";
+import AgentDock from "@/components/agent-dock/AgentDock";
+// import SubAgentChatMessage from "/Users/jonah/.pi/agent/extensions/eu-subagents/src/components/SubAgentChatMessage";
 
 const AgentsPage = () => {
   return (
     <AgentsProvider>
       <AgentAppFrame>
         <AgentChat />
+        {/* <AgentDock /> */}
       </AgentAppFrame>
     </AgentsProvider>
   );
